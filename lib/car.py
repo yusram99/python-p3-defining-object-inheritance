30f191f298baa8a7268040ffaf0a2f3f324a74e0
Your Name <youremail@example.com>
2023-08-22 12:02:17 +0300

    completed assignment

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,11 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+     def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+        
+class Car(Vehicle):
+  def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
